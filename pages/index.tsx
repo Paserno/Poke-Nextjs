21f0476f8751b8c09a3e2c31e1a3109ba786f890
Paserno <paserno.x@gmail.com>
2023-06-20 04:48:12 -0400

    componente card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { NextPage, GetStaticProps } from 'next'
-import { Card, Grid, Row, Text } from '@nextui-org/react';
 
 import { Layout } from '../components/layouts'
 import { pokeApi } from '../api';
 import { PokemonListResponse, SmallPokemon } from '../interfaces';
+import { Grid } from '@nextui-org/react';
+import { PokemonCard } from '../pokemon';
 
 interface Porps {
   pokemons: SmallPokemon[];
@@ -12,7 +13,7 @@ interface Porps {
 
 const HomePage: NextPage<Porps> = ({ pokemons }) => {
 
-  console.log(pokemons);
+  // console.log(pokemons);
 
 
 
@@ -22,26 +23,8 @@ const HomePage: NextPage<Porps> = ({ pokemons }) => {
 
       <Grid.Container gap={2} justify='flex-start'>
         {
-          pokemons.map(({ id, name, img }) => (
-            <Grid xs={6} sm={3} md={2} xl={1}>
-              <Card isHoverable isPressable>
-                <Card.Body css={{ p: 1 }}>
-                  <Card.Image
-                    src={img}
-                    width="100%"
-                    height={ 140 }
-                  />
-                </Card.Body>
-                <Card.Footer>
-                  <Row justify='space-between'>
-                    <Text transform='capitalize'>{name}</Text>
-                    <Text>#{id}</Text>
-
-                  </Row>
-                </Card.Footer>
-              </Card>
-
-            </Grid>
+          pokemons.map((pokemon) => (
+            <PokemonCard pokemon={pokemon} key={pokemon.id} />
           ))
         }
 
@@ -59,13 +42,12 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
 
   const { data } = await pokeApi.get<PokemonListResponse>('/pokemon/?limit=151');
 
-  console.log(data);
+  // console.log(data);
   const pokemons: SmallPokemon[] = data.results.map((poke, i) => ({
     ...poke,
     id: i + 1,
     img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i + 1}.svg`
   }));
-  // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg
 
 
   return {
---
+++ pokemon/PokemonCard.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react"
+import { SmallPokemon } from "../interfaces"
+import { Card, Grid, Row, Text } from '@nextui-org/react';
+
+
+interface Props {
+    pokemon: SmallPokemon
+}
+
+export const PokemonCard: FC<Props> = ({ pokemon }) => {
+    const { name, id, img } = pokemon;
+    
+    return (
+        <Grid xs={6} sm={3} md={2} xl={1}>
+            <Card isHoverable isPressable>
+                <Card.Body css={{ p: 1 }}>
+                    <Card.Image
+                        src={img}
+                        width="100%"
+                        height={140}
+                    />
+                </Card.Body>
+                <Card.Footer>
+                    <Row justify='space-between'>
+                        <Text transform='capitalize'>{name}</Text>
+                        <Text>#{id}</Text>
+
+                    </Row>
+                </Card.Footer>
+            </Card>
+
+        </Grid>
+    )
+}
